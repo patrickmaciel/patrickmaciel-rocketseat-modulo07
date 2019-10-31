3b242fcf949a49100e96854b41d253cd09da7148
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-31 20:21:30 -0300

    estilos globais

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
-// import GlobalStyle from './styles/global';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <BrowserRouter>
       {/* <Header /> */}
       <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
