9aa3f51a24a522b6bcef5229a8e1dab3078fddbf
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-31 21:55:00 -0300

    buscando produtos da api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
 import { ProductList } from './styles';
 
 import tenis from '../../assets/images/tenis.png';
 
-export default function Home() {
-  return (
-    <ProductList>
-      {[...Array(6).keys()].map((e, i) => (
-        <li key={i}>
-          <img src={tenis} alt="Tênis" />
-          <strong>Tênis muito legal</strong>
-          <span>R$129,90</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#fff" /> 3
-            </div>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-      ))}
-    </ProductList>
-  );
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
