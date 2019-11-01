e3ad012735da9654a0b2885a5891a2c8b53b04bd
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-31 21:04:08 -0300

    estilização da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
+
+import tenis from '../../assets/images/tenis.png';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      {[...Array(6).keys()].map((e, i) => (
+        <li key={i}>
+          <img src={tenis} alt="Tênis" />
+          <strong>Tênis muito legal</strong>
+          <span>R$129,90</span>
+
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" /> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
+  );
 }
