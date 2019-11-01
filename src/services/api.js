3be6f40840333c138dc6f4bea56e8b66417e4751
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-31 21:46:04 -0300

    configurando api fake mock

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.github.com',
-  headers: {
-    // Authorization: '620cd6dc28e6754c927f38c55fda93b15ce5d1b2',
-    Authorization: 'Bearer 15321beb2661bf0bd41a98b20d99fd14dd97877a',
-  },
+  baseURL: 'https://localhost:3333',
 });
 
 export default api;
