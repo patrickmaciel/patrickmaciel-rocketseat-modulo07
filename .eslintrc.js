d9a89e144367e1b95d2c159c58aacb6b70cbd67a
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-11-01 21:03:38 -0300

    produto duplicado

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
     'react/prefer-stateless-function': 'off',
     'react/state-in-constructor': 'off',
     'react/static-property-placement': 'off',
-    'no-console': ['error', { allow: ['tron'] }]
+    'no-console': ['error', { allow: ['tron'] }],
+    'no-param-reassign': 'off',
   },
 };
