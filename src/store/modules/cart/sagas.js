589bca22d83d4494d6e7c53e8dc5cd89dd59555c
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-11-01 22:04:17 -0300

    react toastify

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,4 +1,5 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import { addToCartSuccess, updateAmount } from './actions';
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format';
@@ -14,7 +15,7 @@ function* addToCart({ id }) {
   const amount = currentAmount + 1;
 
   if (amount > stockAmount) {
-    console.tron.warning('ERRO');
+    toast.error('Quantidade solicitada fora do estoque');
     return;
   }
 
